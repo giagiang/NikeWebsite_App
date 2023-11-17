cc0554ccd2adfeb0b9d3b1b4b156efb3384558e2
fastboy <giagiang200@gmail.com>
2023-11-17 12:00:36 +0700

    add button in hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import {statistics} from "../constants";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          {" "}
+          Our Summer Collection
+        </p>-
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike </span>
+          Shoes
+        </h1>
+        <p>
+          {" "}
+          Discover stylish Nike arrival, quality comfort, and innovation for
+          your active life.
+        </p>
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+              <p className='leading-7 font-montserrat text-slate-gray'>
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;

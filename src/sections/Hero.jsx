711dec08856652d8cbf926873a6e0ab614ad2340
fastboy <giagiang200@gmail.com>
2023-11-22 22:50:19 +0700

    create shoe Quality

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import ShoeCard from "../components/ShoeCard";
 import { arrowRight } from "../assets/icons";
@@ -49,18 +49,19 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
 
-
-      <div className="flex sm:gap-6 gap-4 absolute bottom-[-5%] sm:left-[10%] max-sm:px-6">
-        {shoes.map((shoe) => (
-          <div key={shoe}>
-            <ShoeCard
-              imgURL={shoe}
-              changeBigShoeImage={(shoe) => {setBigShoeImg(shoe)}}
-              bigShoeImg = {bigShoeImg}
-            />
-          </div>
-        ))}
-      </div>
+        <div className="flex sm:gap-6 gap-4 absolute bottom-[-5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => {
+                  setBigShoeImg(shoe);
+                }}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );

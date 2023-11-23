a6605969596bbb7e8afbf98af9661847f6b87dc2
fastboy <giagiang200@gmail.com>
2023-11-23 18:20:27 +0700

    fix flexbox

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      {iconURL&& <img
-          src={iconURL}
+      {iconUrl && <img
+          src={iconUrl}
           alt="arrow right icon"
           className="ml-2 rounded-full w-5 h-5"
         />}

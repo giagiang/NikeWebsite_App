e8009d99c379bd4c9fa13cc5dd039b1e7be1696b
fastboy <giagiang200@gmail.com>
2025-03-21 22:30:26 +0700

    commit

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import { star } from 'src/assets/icons'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { star } from 'src/assets/icons';
 
-const PopularProductCard = ({imgURL,name, price}) => {
+const PopularProductCard = ({ imgURL, name, price }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/product/${name}`);
+  };
+
   return (
-    <div className='flex flex-1 flex-col w-full max-sm:w-full'>
+    <div className='flex flex-col flex-1 w-full cursor-pointer max-sm:w-full' onClick={handleClick}>
       <img 
-        src = {imgURL} alt = {name} className="w-[280px] h-[280-px]"
+        src={imgURL} alt={name} className="w-[280px] h-[280-px]"
       />
-      <div className =  "mt-8 flex justify-start gap-2.5">
-        <img src ={star} alt = "rating" width = {24}  height={24}/>
-        <p className="font-montserrat text-xl leading-normal">(5.0)</p>
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="text-xl leading-normal font-montserrat">(5.0)</p>
       </div>
-      <h3 className='mt-2 text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
-      <p className='mt-2 font-semibold font-montserrat text-coral-red text-2lx leading-normal'> {price}</p>
-    </div> 
-  ) 
-}
+      <h3 className='mt-2 text-2xl font-semibold leading-normal font-palanquin'>{name}</h3>
+      <p className='mt-2 font-semibold leading-normal font-montserrat text-coral-red text-2lx'>{price}</p>
+    </div>
+  );
+};
 
-export default PopularProductCard
+export default PopularProductCard;
